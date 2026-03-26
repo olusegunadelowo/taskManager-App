e8bc4896a98de6a7436f5d1ff28157bddb97fbc8
JFKej <olusegunadelowo@yahoo.com>
2026-03-26 11:06:02 +0100

    Revert to sqlite3

--- taskscheduler/settings.py
+++ taskscheduler/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'taskscheduler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'E  NGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 #DATABASES = {
     #'default': {
