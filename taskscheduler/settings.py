62a81764f648cb7fafc40cd2adf280ccc99ed4db
JFKej <olusegunadelowo@yahoo.com>
2026-03-26 12:50:37 +0100

    Changed DB to PosgreSQL

--- taskscheduler/settings.py
+++ taskscheduler/settings.py
@@ -92,24 +92,24 @@ WSGI_APPLICATION = 'taskscheduler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
     #'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'tasksappDB',
-        #'USER': 'postgres',
-        #'PASSWORD': 'Ayonitemi!1',
-        #'HOST': 'localhost',
-        #'PORT': '5433',
-       
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     #}
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tasksappDB',
+        'USER': 'postgres',
+        'PASSWORD': 'Ayonitemi!1',
+        'HOST': 'localhost',
+        'PORT': '5433',
+       
+    }
+}
 
 
 # Password validation
