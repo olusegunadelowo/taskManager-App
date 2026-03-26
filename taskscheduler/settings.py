7cfbbf4d96f3bf95cf3c5e463680ecd3005129c9
JFKej <olusegunadelowo@yahoo.com>
2026-03-26 11:22:32 +0100

    dm modified

--- taskscheduler/settings.py
+++ taskscheduler/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'taskscheduler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'E  NGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
